48ce122a5da7be4eec8619417ee80f2619d00748
DESKTOP-3SSGUTH\whdkf <whdkfk4012@naver.com>
2021-08-12 23:29:14 +0900

    완료

---
+++ CH03_06/app.js
@@ -0,0 +1,85 @@
+const container = document.getElementById('root');
+const ajax = new XMLHttpRequest();
+const content = document.createElement('div');
+const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
+const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
+const store = {
+    currentPage: 1,
+};
+
+function getData(url){
+    ajax.open('GET', url, false);
+    //false 동기, true 비동기
+    ajax.send();
+
+    return JSON.parse(ajax.response);
+}
+
+function newsDetail(){
+  const id = location.hash.substr(7);
+  const newContent =  getData(CONTENT_URL.replace('@id',id));
+
+  const title = document.createElement('h1');
+
+
+  container.innerHTML = `
+  <h1>${newContent.title}</h1>
+
+  <div>
+    <a href="#/page/${store.currentPage}">목록으로</a>
+    </div>
+  ` ;
+}
+
+function newsFeed(){
+    let currentPage = 1;
+    const newsFeed = getData(NEWS_URL);
+    const newsList = [];
+
+    let template = `
+     <div class="container mx-auto p-4">
+        <h1>Kacker News</h1>
+        <ul>
+            {{__news_feed__}}
+        </ul>
+        <div>
+            <a href="#/page/{{__prev_page__}}">이전 페이지</a>
+            <a href="#/page/{{__next_page__}}">다음 페이지</a>
+        </div>
+    </idv>
+    `;
+
+
+    newsList.push('<ul>');
+
+    for(let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++){
+    newsList.push( `
+    <li>
+        <a href="#/show/${newsFeed[i].id}">
+        ${newsFeed[i].title} (${newsFeed[i].comments_count})
+        </a>
+    </li>
+    `);
+    }
+    template = template.replace('{{__news_feed__}}', newsList.join(''));
+    template = template.replace('{{__prev_page__}}', store.currentPage > 1 ? store.currentPage -1 : 1);
+    template = template.replace('{{__next_page__}}', store.currentPage + 1);
+    container.innerHTML = template;   
+}
+
+function router(){
+    const routePath = location.hash;
+
+    if(routePath === ''){
+        newsFeed();
+    }else if(routePath.indexOf('#/page/') >= 0){
+        store.currentPage = Number(routePath.substr(7));
+        newsFeed();
+    }else {
+        newsDetail();
+    }
+}
+
+
+window.addEventListener('hashchange', router);
+router();
